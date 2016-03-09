1dd8031370326afbdf4573d217563c86899abe57
travis14 <travis14@stanford.edu>
2016-03-09 15:12:25 -0800

    updated to return lead info

--- scrapePlayByPlay.py
+++ scrapePlayByPlay.py
@@ -14,7 +14,7 @@ GamePlayInfo = collections.namedtuple("GamePlayInfo", ["HomeTeam", "AwayTeam", "
 def getPlayFromGameID(gameID):
 	gameSoup = BeautifulSoup(urlopen(playByPlayBaseURL + str(gameID)), "lxml")
 	awayTeam = gameSoup.find_all("a", {"name": "&lpos=nba:game:playbyplay:clubhouse:team"})[1].find_all(text=True)[2]
-	homeTeam = gameSoup.find_all("a", {"name": "&lpos=nba:game:playbyplay:clubhouse:team"})[3].find_all(text=True)[2]
+	homeTeam = gafmeSoup.find_all("a", {"name": "&lpos=nba:game:playbyplay:clubhouse:team"})[3].find_all(text=True)[2]
 	finalAwayScore = int(gameSoup.find("div", {"class": "score icon-font-after"}).find(text=True))
 	finalHomeScore = int(gameSoup.find("div", {"class": "score icon-font-before"}).find(text=True))
 	finalDelta = finalHomeScore - finalAwayScore
@@ -33,6 +33,38 @@ def getPlayFromGameID(gameID):
 
 	return GamePlayInfo(HomeTeam = homeTeam, AwayTeam = awayTeam, SecondsLeft = timeStampSecondsLeft, PlayDelta = timeStampDelta, FinalDelta = finalDelta)
 
-print getPlayFromGameID(400828836)
+def getMaxFourthQuarterLeads(gameID):
+	info = []
+	gameSoup = BeautifulSoup(urlopen(playByPlayBaseURL + str(gameID)), "lxml")
+	awayTeam = gameSoup.find_all("a", {"name": "&lpos=nba:game:playbyplay:clubhouse:team"})[1].find_all(text=True)[2]
+	homeTeam = gameSoup.find_all("a", {"name": "&lpos=nba:game:playbyplay:clubhouse:team"})[3].find_all(text=True)[2]
+	finalAwayScore = int(gameSoup.find("div", {"class": "score icon-font-after"}).find(text=True))
+	finalHomeScore = int(gameSoup.find("div", {"class": "score icon-font-before"}).find(text=True))
+	finalDelta = finalHomeScore - finalAwayScore
 
+	fourthQuarterDiv = gameSoup.find("div", {"id": "gp-quarter-4"})
+	plays = fourthQuarterDiv.find_all("tr")
+	maxHomeLead = float('-inf')
+	maxAwayLead = float('-inf')
+	for play in plays:
+		playScore = play.find("td", {"class": "combined-score no-change"})
+		if playScore:
+			playScore = playScore.find(text=True)
+			playAwayScore = int(playScore.split('-')[0])
+			playHomeScore = int(playScore.split('-')[1])
+			homeLead = playHomeScore - playAwayScore
+			if homeLead > maxHomeLead:
+				maxHomeLead = homeLead
+			awayLead = playAwayScore - playHomeScore
+			if awayLead > maxAwayLead:
+				maxAwayLead = awayLead
+	return [homeTeam, awayTeam, maxHomeLead, maxAwayLead, finalDelta]
 
+#print getPlayFromGameID(400828836)
+#print getMaxFourthQuarterLeads(400828836)
+leadInfos = []
+for i in range(400827890, 400828836 + 1):
+	leadInfo = getMaxFourthQuarterLeads(i)
+	leadInfos.append(leadInfo)
+	print leadInfo
+#first game of the season is ID=400827890, most recent one as of 3/9 is ID=400828836
